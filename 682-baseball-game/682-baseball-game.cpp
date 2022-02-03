5ed75c4b4f573b660ca5d21dd86431fffac8f4fb
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-02-03 08:34:50 +0530

    Time: 11 ms (13.86%), Space: 8.4 MB (26.36%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        vector<int>s;
+      
+        for(auto c: ops){char ch=c[0];
+             if(ch=='C')s.erase(s.end()-1);
+            else if(ch=='D')s.push_back(s[s.size()-1]*2);
+            else if(ch=='+')s.push_back(s[s.size()-1]+s[s.size()-2]);
+            else s.push_back(stoi(c));
+                           
+        }
+            return accumulate(s.begin(),s.end(),0);
+        }
+};
