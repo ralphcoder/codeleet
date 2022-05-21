a244b6e8230101afc60c6b238f077d40fca393aa
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-05-21 08:03:24 +0530

    Time: 101 ms (12.99%), Space: 24.3 MB (28.09%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+         deque <int> a;int b=0;
+        for(int i=1;i<=n;i++)a.push_back(i);
+        for(int i=1;a.size()!=1;i++){
+            if(i%k==0)a.pop_front();
+          else
+          {  b=a.front();
+              a.pop_front();
+             a.push_back(b);
+         }
+        }
+        return a.front();
+    }
+};
