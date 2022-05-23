ce6ce391205baaf46bfdc59f9a4fbbe831956b0c
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-05-23 15:19:37 +0530

    Time: 143 ms (96.81%), Space: 92.8 MB (86.55%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int mt) {
-       vector<int>ans;int si=nums.size()-1,xo=nums[0], t=pow(2,mt)-1;
-        for(int i=1;i<nums.size();i++){
-            ans.push_back(xo^t);
-            cout<<xo<<" ";
+        int s=nums.size();
+       vector<int>ans(s,0);int si=nums.size()-1,xo=nums[0], t=pow(2,mt)-1;
+        for(int i=1;i<s;i++){
+            ans[s-i]=(xo^t);
             xo^=nums[i];
-        } ans.push_back(xo^t);
-        reverse(ans.begin(),ans.end());
+        } 
+        ans[0]=(xo^t);
         return ans;
     }
 };
