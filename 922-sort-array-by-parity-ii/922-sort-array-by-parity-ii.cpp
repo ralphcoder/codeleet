99707da485708f5b70cb1fd706ce1a28ae8a79f9
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-06-17 15:44:07 +0530

    Time: 21 ms (90.75%), Space: 21.3 MB (88.61%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        vector<int>ans(nums.size(),100);
+        vector<int>ans(nums.size(),0);
         int a=0,j=1;
         for(auto i:nums){
             if(i&1)ans[j]=i,j+=2;
