578390d1212f0d0fb4ae48169809516d355f0ff0
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-07-07 15:03:02 +0530

    Time: 10 ms (50.96%), Space: 8.3 MB (42.30%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = cur;
+            cur = head;
+            head = next;
+        }
+        return cur;
+    }
+};
