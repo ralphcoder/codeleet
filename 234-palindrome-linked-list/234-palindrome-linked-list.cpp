dc9c271d165539a353c89e12371ff0ef6710339b
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-07-29 21:34:25 +0530

    Time: 389 ms (40.03%), Space: 128.3 MB (13.28%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,22 +11,16 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-      int n=0,c=0;
+      int c=0;
         vector<int>a;
         while(head)
         {
            a.push_back(head->val);
             head=head->next;
             c++;
-        } 
-        int left=0,right=(c+1)/2;
-        if(c%2==0)  left=(c/2)-1;
-        else left=(c/2)-1,c=c-1;
-        c=c/2;
-        
-        while(c){
-            cout<<a[left]<<" "<<a[right]<<"--";
-            if(a[left--]!=a[right++])return false;c--;}
+        } int b=c-1;
+        for(int i=0;i<c/2;i++,b--)
+        if(a[i]!=a[b])return false;
         return true;
     }
 };
