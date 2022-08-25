957a9ebab833b565f07d0c98a9265f55d92ab52c
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-08-25 09:09:56 +0530

    Time: 5 ms (99.45%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    bool canConstruct(string ran, string mag) {
-        map<char,int>a;
-        for(char ch:mag)a[ch]++;
-        for(char ch:ran){
-            if(!a[ch] || a[ch]--<0)return false;
-        }
+   bool canConstruct(string ransomNote, string magazine) {
+        
+        int count[26] = {0};
+        for(char ch : magazine)
+            count[ch - 'a']++;
+        
+        for(char ch : ransomNote)
+            if(count[ch - 'a']-- <= 0)
+                return false;
+        
         return true;
     }
 };
