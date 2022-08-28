e77c49d6261308f873314ba2520b17083f7c3e93
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-08-28 21:22:05 +0530

    Time: 12 ms (69.10%), Space: 12 MB (62.29%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -8,8 +8,16 @@ public:
         a.push_back(t);}
         for(string y:words)
         {
-            if(y.size()<=s.size() && a[y.size()-1]==y)ans++;
+            if(y.size()<=s.size() && a[y.size()-1]==y)ans++;//hence proved if we order a value grater than vector size it gives runtime exception (rather than handling it)
      }
         return ans;
     }
 };
+/*
+    int countPrefixes(vector<string>& words, string s) {
+        int res = 0;
+        for (auto& w : words)
+            res += s.find(w) < 1;
+        return res; 
+    }
+    */
