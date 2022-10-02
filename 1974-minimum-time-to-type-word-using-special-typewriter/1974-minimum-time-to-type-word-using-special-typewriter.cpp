2012d7ef307029c55fbc2fdf3334fe3023063dc5
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-10-02 19:55:24 +0530

    Time: 4 ms (31.72%), Space: 6.2 MB (46.27%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -3,10 +3,8 @@ public:
     int minTimeToType(string w,int ans=0) {
         
         if(w[0]!='a')w='a'+w;
-        else ans=1;
-        cout<<w;
+        else ans=1;                                 
         for(int i=1;i<w.size();i++){
-          cout<<abs(w[i]-w[i-1])<<" "<<abs(w[i-1]-97)+abs(123-w[i])<<" "<<abs(w[i]-97)+abs(123-w[i-1])<<"  ";
             ans+=min(abs(w[i]-w[i-1]),min(abs(w[i-1]-97)+abs(123-w[i]),abs(w[i]-97)+abs(123-w[i-1])));
             ans++;
         }
