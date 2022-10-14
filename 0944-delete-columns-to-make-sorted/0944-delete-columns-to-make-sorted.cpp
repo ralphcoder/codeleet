7149f0a3edc06ae06c24dce461fe9c6b1d4eaa30
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-10-14 07:40:59 +0530

    Time: 49 ms (89.76%), Space: 12.2 MB (71.88%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& s,int ans=0) {
+   for(int i=0;i<s[0].size();i++)
+   for(int j=1;j<s.size();j++)
+       if(s[j-1][i]>s[j][i]){ans++;break;}
+    
+   return ans;
+    }
+};
