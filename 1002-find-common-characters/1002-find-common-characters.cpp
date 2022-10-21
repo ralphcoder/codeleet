97eecaeb7a4b8a22cb7f12b1a01468c2fd1e3a80
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2022-10-21 20:59:02 +0530

    Time: 62 ms (7.14%), Space: 11.4 MB (20.12%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& w) {
-        unordered_map<char,int>a;
         unordered_map<char,int>c;
         vector<string>ans;
-        for(char ch:w[0])a[ch]++,c[ch]++;
+        for(char ch:w[0])c[ch]++;
         for(int i=1;i<w.size();i++)
         {  unordered_map<char,int>b;
             for(char ch:w[i])
